7ba84f12b3caaf96e94f5ecb88a7be56d102868d
HgSO4QwQ <felderzhang@163.com>
2024-11-15 07:54:01 +0800

    added rating graph

--- makeInfo.cpp
+++ makeInfo.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 typedef long long ll;
-const int N=100010,C=8;
+const int N=100010,C=Llim;
 string Date[N];double R[N];
 ll T[N];
 struct Pt {double x,y;}P[N];
@@ -55,6 +55,7 @@ int main(int argc,char *argv[])
     for(int i=1;i<=n;i++) cin>>R[i]>>Date[i]>>T[i];
     double l1=1e18,r1=0;
     for(int i=1;i<=n;i++) r1=max(r1,R[i]),l1=min(l1,R[i]);
+    double lllll=l1,rrrrr=r1;
     if(r1<1422) r1=1422;
     if(l1>1378) l1=1378;
     double x11=35,x12=heig-35;
@@ -69,16 +70,18 @@ int main(int argc,char *argv[])
         P[i]={(T[i]-r2+k2*x21)/k2,(R[i]-r1+k1*x11)/k1};
         cerr<<'('<<T[i]<<','<<R[i]<<")->("<<P[i].x<<','<<P[i].y<<')'<<endl;
     }
-    lim[1]={   1, 850,"#80808085","#808080","Retard"};
-    lim[2]={ 850,1050,"#80400085","#804000","Newbie"};
-    lim[3]={1050,1250,"#00800085","#008000","Pupil"};
-    lim[4]={1250,1400,"#03A89E85","#03A89E","Specialist"};
-    lim[5]={1400,1550,"#0000FF85","#0000FF","Expert"};
-    lim[6]={1550,1750,"#AA00AA85","#AA00AA","Candidate Master"};
-    lim[7]={1750,1950,"#FF8C0085","#FF8C00","Master"};
-    lim[8]={1950,2550,"#FF000085","#FF0000","Grandmaster"};
-    pair<string,string> YouSIR;
-    for(int i=1;i<=8;i++) if(lim[i].L<=R[n]&&R[n]<=lim[i].R) YouSIR={lim[i].c2,lim[i].rnknm};
+    int Llim=0;
+    lim[++Llim]={   1, 850,"#80808085","#808080","Retard"};
+    lim[++Llim]={ 850,1100,"#80400085","#804000","Newbie"};
+    lim[++Llim]={1100,1300,"#00800085","#008000","Pupil"};
+    lim[++Llim]={1300,1400,"#03A89E85","#03A89E","Specialist"};
+    lim[++Llim]={1400,1550,"#0000FF85","#0000FF","Expert"};
+    lim[++Llim]={1550,1750,"#AA00AA85","#AA00AA","Candidate Mastar"};
+    lim[++Llim]={1750,1950,"#FF8C0085","#FF8C00","Mastar"};
+    lim[++Llim]={1950,2550,"#FF000085","#FF0000","Superman"};
+    pair<string,string> YouSIR,YouMAXSIR;
+    for(int i=1;i<=Llim;i++) if(lim[i].L<=R[n]&&R[n]<=lim[i].R) YouSIR={lim[i].c2,lim[i].rnknm};
+    for(int i=1;i<=Llim;i++) if(lim[i].L<=rrrrr&&rrrrr<=lim[i].R) YouMAXSIR={lim[i].c2,lim[i].rnknm};
 
 
 
@@ -94,10 +97,13 @@ cout<<R"aaa(
 <body>
 )aaa"<<endl;
 cout<<"    <div class=\"header\">\n";
-printf("        <div class=\"nag\" style=\"width: %.0lfpx;margin: auto;\">\n",wid);printf("\n");
+printf("        <div class=\"nag\" style=\"width: %.0lfpx;margin: auto;\">\n",wid-50);printf("\n");
 printf("            <span style=\"font-size:10px;color: %s;\">%s</span>",YouSIR.first.c_str(),YouSIR.second.c_str());printf("\n");
 printf("            <p style=\"font-family: 'Noto Mono';font-size:35px;margin-top: 0em;margin-bottom:0em;color: %s; \"><b>%s</b></p>\n",YouSIR.first.c_str(),Name.c_str());printf("\n");
-printf("            <p >  Rating: <b style=\"color: %s\">%.0lf</b></p>\n",YouSIR.first.c_str(),R[n]);printf("\n");
+printf("            <p style=\"font-size:22px\">  Rating: <b style=\"color: %s\">%.0lf</b></p>\n",YouSIR.first.c_str(),R[n]);printf("\n");
+printf("            <p >  Max: <b style=\"color: %s\">%s, %.0lf</b></p>\n",YouMAXSIR.first.c_str(),YouMAXSIR.second.c_str(),rrrrr);printf("\n");
+printf("        </div>");printf("\n");
+printf("        <div class=\"grph\" style=\"width: %.0lfpx;margin: auto;\">\n",wid);
 printf("            <canvas id=\"graph\" width=\"%.0lf\" height=\"%.0lf\"> </canvas>",wid,heig+20);printf("\n");
 printf("            <script>");printf("\n");
 printf("                var c = document.getElementById(\"graph\");");printf("\n");
@@ -111,7 +117,7 @@ printf("                var pnt1_2 = c.getContext(\"2d\");");printf("\n");
     for(int i=1;i<=n;i++) cerr<<R[i]<<' '<<Date[i]<<endl;
     init("ptr1");init("lln1");init("fnt1");cout<<endl;
     cerr<<k1<<endl;
-    for(int i=1;i<=8;i++)
+    for(int i=1;i<=Llim;i++)
     {
         double L=(lim[i].L-r1+k1*x11)/k1;
         // k1*lim[i].L-k1*x11+r1;
